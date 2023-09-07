b6c7fe41a1a7b4fb945949ec766a094cce2c6f81
Ali Mirzaei <98237451+Ali-mirzayi@users.noreply.github.com>
2023-09-07 22:57:51 +0330

    handleError
    when not assign env

--- src/fetcher/fetcher.tsx
+++ src/fetcher/fetcher.tsx
@@ -1,7 +1,28 @@
-export async function getData() {
+import { Data } from "@/types/types"
+
+const emptyMockData:Data = {
+  "posts": [{
+      "id": 0,
+      "slug": "",
+      "title": "",
+      "excerpt": "",
+      "imageUrl": "",
+      "categories": [0]
+    }],
+  "categories": [{
+      "id": 0,
+      "name": "",
+      "slug": ""
+  }]} 
+
+export async function getData():Promise<Data> {
+  if (process.env.BASE_URL){
     const res = await fetch(process.env.BASE_URL+'/api')
      if (!res.ok) {
       throw new Error('Failed to fetch')
     }
     return res.json()
+   }else{
+    return emptyMockData
+   }
   }
