ac77e6d8bf404de5421ecbae429e4a09ea126905
Ali Mirzaei <98237451+Ali-mirzayi@users.noreply.github.com>
2023-09-08 00:27:17 +0330

    Update fetcher.tsx

--- src/fetcher/fetcher.tsx
+++ src/fetcher/fetcher.tsx
@@ -1,28 +1,9 @@
 import { Data } from "@/types/types"
 
-const emptyMockData:Data = {
-  "posts": [{
-      "id": 0,
-      "slug": "",
-      "title": "",
-      "excerpt": "",
-      "imageUrl": "",
-      "categories": [0]
-    }],
-  "categories": [{
-      "id": 0,
-      "name": "",
-      "slug": ""
-  }]} 
-
 export async function getData():Promise<Data> {
-  if (process.env.BASE_URL){
     const res = await fetch(process.env.BASE_URL+'/api')
      if (!res.ok) {
       throw new Error('Failed to fetch')
     }
     return res.json()
-   }else{
-    return emptyMockData
-   }
-  }
+}
